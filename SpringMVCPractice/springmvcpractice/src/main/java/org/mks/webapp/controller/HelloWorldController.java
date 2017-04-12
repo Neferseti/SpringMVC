775bb28403264abe04acc9edcf9a7969b38bccba
ks_mani <kumar.mani017@gmail.com>
2017-04-12 13:22:45 +0530

    2: Modified HelloWorldController and dispatcher-servlet.xml to run the app using annotations

--- SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/HelloWorldController.java
+++ SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/HelloWorldController.java
@@ -1,18 +1,16 @@
 package org.mks.webapp.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-public class HelloWorldController extends AbstractController {
+@Controller
+public class HelloWorldController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
-		// TODO Auto-generated method stub
+	@RequestMapping("/welcome.html")
+	public ModelAndView helloWorld(){
 		ModelAndView modelAndView = new ModelAndView("HelloWorldPage");
-		modelAndView.addObject("welcomeMessage", "Hello Mani!");
+		modelAndView.addObject("welcomeMessage", "Hello Mani! Welcome to the world of annotations");
 		return modelAndView;
 	}
 
