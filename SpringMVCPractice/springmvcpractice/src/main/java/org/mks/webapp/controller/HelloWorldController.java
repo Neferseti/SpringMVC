11e0278b09b456be734406c3dbbf0e7ec5403f5d
ks_mani <kumar.mani017@gmail.com>
2017-04-12 15:59:47 +0530

    3: Changes in HelloWorldController For Multi Action Controllers

--- SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/HelloWorldController.java
+++ SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/HelloWorldController.java
@@ -5,12 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/greet")
 public class HelloWorldController {
 
-	@RequestMapping("/welcome.html")
+	@RequestMapping("/welcome")
 	public ModelAndView helloWorld(){
 		ModelAndView modelAndView = new ModelAndView("HelloWorldPage");
-		modelAndView.addObject("welcomeMessage", "Hello Mani! Welcome to the world of annotations");
+		modelAndView.addObject("welcomeMessage", "Hello World");
+		return modelAndView;
+	}
+	
+	@RequestMapping("hi")
+	public ModelAndView hiWorld(){
+		ModelAndView modelAndView = new ModelAndView("HelloWorldPage");
+		modelAndView.addObject("welcomeMessage", "Hi World");
 		return modelAndView;
 	}
 
