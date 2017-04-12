10b633339731d8968be58a5a47c632fbf1206f26
ks_mani <kumar.mani017@gmail.com>
2017-04-12 16:29:06 +0530

    4: For usage of path variables, changes in HelloWorldController and dispatcher-servlet.xml (mvc annotation driven)

--- SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/HelloWorldController.java
+++ SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package org.mks.webapp.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,11 +18,27 @@ public class HelloWorldController {
 		return modelAndView;
 	}
 	
-	@RequestMapping("hi")
+	@RequestMapping("/hi")
 	public ModelAndView hiWorld(){
 		ModelAndView modelAndView = new ModelAndView("HelloWorldPage");
 		modelAndView.addObject("welcomeMessage", "Hi World");
 		return modelAndView;
 	}
 
+	@RequestMapping("/welcome/{username}")
+	public ModelAndView helloUser(@PathVariable("username") String username){
+		ModelAndView modelAndView = new ModelAndView("HelloWorldPage");
+		modelAndView.addObject("welcomeMessage", "Hello "+username);
+		return modelAndView;
+	}
+	
+	@RequestMapping("/welcome/{username}/{country}")
+	public ModelAndView helloUser(@PathVariable Map<String, String> pathVar){
+		String username=pathVar.get("username");
+		String country=pathVar.get("country");
+		ModelAndView modelAndView = new ModelAndView("HelloWorldPage");
+		modelAndView.addObject("welcomeMessage", "Hello "+username+". You are from "+country);
+		return modelAndView;
+	}
+	
 }
