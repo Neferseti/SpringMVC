ec331c64cc324b0cbd83c79b48fe3f7a4f3bc165
ks_mani <kumar.mani017@gmail.com>
2017-04-13 12:12:47 +0530

    6: @ModelAttribute annotation at Method level. Changes in StudentAdmissionController.java and AdmissionForm.jsp

--- SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/StudentAdmissionController.java
+++ SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/StudentAdmissionController.java
@@ -2,10 +2,10 @@ package org.mks.webapp.controller;
 
 import org.mks.webapp.entity.StudentEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,12 +16,16 @@ public class StudentAdmissionController {
 		ModelAndView modelAndView = new ModelAndView("AdmissionForm");
 		return modelAndView;
 	}
-
+	
+	@ModelAttribute
+	public void addingCommonObjects(Model model1){
+		model1.addAttribute("headerMessage","College of Engineering");
+	}
+	
 	@RequestMapping(value = "submitAdmissionForm.html", method = RequestMethod.POST)
 	public ModelAndView submitAdmissionForm(@ModelAttribute("student1") StudentEntity student1) {
 		
 		ModelAndView modelAndView = new ModelAndView("AdmissionSuccess");
-		modelAndView.addObject("headerMessage", "Welcome!");
 		return modelAndView;
 	}
 }
--- SpringMVCPractice/springmvcpractice/src/main/webapp/WEB-INF/AdmissionForm.jsp
+++ SpringMVCPractice/springmvcpractice/src/main/webapp/WEB-INF/AdmissionForm.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" isELIgnored="false"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +7,7 @@
 <title>Admission Form</title>
 </head>
 <body>
+	<h1>${headerMessage}</h1>
 	<h1>Student Admission Form</h1>
 	<form action="/springmvcpractice/submitAdmissionForm.html" method="post">
 		<p>
