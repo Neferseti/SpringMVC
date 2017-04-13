cee4dfb51f20cd7e511fe7355c2a20adc4a59e76
ks_mani <kumar.mani017@gmail.com>
2017-04-13 11:25:37 +0530

    6: With @ModelAttribute annotation. StudentAdmissionController.java modified

--- SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/StudentAdmissionController.java
+++ SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/StudentAdmissionController.java
@@ -2,6 +2,7 @@ package org.mks.webapp.controller;
 
 import org.mks.webapp.entity.StudentEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,16 +18,10 @@ public class StudentAdmissionController {
 	}
 
 	@RequestMapping(value = "submitAdmissionForm.html", method = RequestMethod.POST)
-	public ModelAndView submitAdmissionForm(@RequestParam("studentName") String name,
-			@RequestParam("studentHobby") String hobby) {
-		
-		StudentEntity student1= new StudentEntity();
-		student1.setStudentName(name);
-		student1.setStudentHobby(hobby);
+	public ModelAndView submitAdmissionForm(@ModelAttribute("student1") StudentEntity student1) {
 		
 		ModelAndView modelAndView = new ModelAndView("AdmissionSuccess");
 		modelAndView.addObject("headerMessage", "Welcome!");
-		modelAndView.addObject("student1", student1);
 		return modelAndView;
 	}
 }
