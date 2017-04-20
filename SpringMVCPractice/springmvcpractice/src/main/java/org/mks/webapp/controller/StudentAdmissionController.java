ebad76d54683f6a20f709879c8414fb01d06393c
ks_mani <kumar.mani017@gmail.com>
2017-04-20 12:21:13 +0530

    7: Data Binding: part2: Error Binding

--- SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/StudentAdmissionController.java
+++ SpringMVCPractice/springmvcpractice/src/main/java/org/mks/webapp/controller/StudentAdmissionController.java
@@ -3,6 +3,7 @@ package org.mks.webapp.controller;
 import org.mks.webapp.entity.StudentEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,7 +24,12 @@ public class StudentAdmissionController {
 	}
 	
 	@RequestMapping(value = "submitAdmissionForm.html", method = RequestMethod.POST)
-	public ModelAndView submitAdmissionForm(@ModelAttribute("student1") StudentEntity student1) {
+	public ModelAndView submitAdmissionForm(@ModelAttribute("student1") StudentEntity student1, BindingResult result) {
+		
+		if(result.hasErrors()){
+			ModelAndView modelAndView = new ModelAndView("AdmissionForm");
+			return modelAndView;
+		}
 		
 		ModelAndView modelAndView = new ModelAndView("AdmissionSuccess");
 		return modelAndView;
